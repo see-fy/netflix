3c58c78c9c766ccb534e15e36da3bd0b4d4982ac
see-fy <106270254+see-fy@users.noreply.github.com>
2022-11-15 01:14:16 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+        '128': '42rem',
+        '130': '48rem'
+      }
+
+    },
+  },
+  plugins: [],
+}
